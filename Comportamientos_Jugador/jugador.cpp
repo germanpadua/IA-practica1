d517caf893056f46ee3869ecdd20fa9d4b365459
germanpadua <german8adaba@gmail.com>
2022-03-10 21:26:55 +0100

    tutorial hecho

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -30,6 +30,60 @@ Action ComportamientoJugador::think(Sensores sensores){
 	cout << "Vida: " << sensores.vida << endl;
 	cout << endl;
 
+	
+	//Actualizar variables de estado
+	switch(ultimaAccion){
+		case actFORWARD:
+			switch(brujula){
+				case 0:
+					fil --;
+				break;
+				case 1:
+					col++;
+				break;
+				case 2:
+					fil ++;
+				break;
+				case 3:
+					col --;
+				break;
+			}
+
+		break;
+
+		case actTURN_L:
+			brujula = (brujula + 3) % 4;
+			girar_derecha = (rand() % 2) == 0;
+		break;
+
+		case actTURN_R:
+			brujula = (brujula + 1) % 4;
+			girar_derecha = (rand() % 2) == 0;
+		break;
+	}
+
+	if(sensores.terreno[0] == 'G' && !bien_situado){
+		bien_situado = true;
+		fil = sensores.posF;
+		col = sensores.posC;
+	}
+	
+	if(bien_situado){
+		mapaResultado[sensores.posF][sensores.posC] = sensores.terreno[0];
+	}
+
+	//Decidir qué acción tomar
+	if((sensores.terreno[2] == 'T' || sensores.terreno[2] == 'S' || sensores.terreno[2] == 'G') && sensores.superficie[2] == '_') {
+		accion = actFORWARD;
+	}else{
+		if(girar_derecha)
+			accion = actTURN_R;
+		else
+			accion = actTURN_L;
+	}
+
+	ultimaAccion = accion;
+
 
 	// Determinar el efecto de la ultima accion enviada
 	return accion;
